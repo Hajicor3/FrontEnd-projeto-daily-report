4df1930927c45c1088a42aebbc71a5b2c2b45852
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2026-08-22 15:03:19 -0300

    feature/hme-page - Criação de component stats-cards e adicionando ele ao component dashboard

---
+++ src/app/core/features/dashboard/components/stats-card/stats-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  imports: [MatIconModule],
+  selector: 'app-stats-card',
+  styleUrl: './stats-card.scss',
+  templateUrl: './stats-card.html',
+})
+export class StatsCard {}
--- src/app/core/features/dashboard/dashboard.ts
+++ src/app/core/features/dashboard/dashboard.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { StatsCard } from "./components/stats-card/stats-card";
 
 @Component({
-  imports: [],
+  imports: [StatsCard],
   selector: 'app-dashboard',
   styleUrl: './dashboard.scss',
   templateUrl: './dashboard.html',
