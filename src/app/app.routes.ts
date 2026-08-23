c93f2396ac018bce07b25f81762c658a3c31f155
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2026-08-23 10:52:23 -0300

    feature/home-page - adição de rotas e ajuste no app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './core/features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./core/features/dashboard/dashboard').then(m => Dashboard),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Dashboard } from "./core/features/dashboard/dashboard";
 import { Slidebar } from "./core/shared/components/slidebar/slidebar";
 
 @Component({
-  imports: [RouterOutlet, Dashboard, Slidebar],
+  imports: [RouterOutlet, Slidebar],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
