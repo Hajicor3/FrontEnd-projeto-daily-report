b2473bd2f3c2d44de2e3faaf44dbcd12296729e1
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2026-08-23 11:24:35 -0300

    feature/atividade-pages - criação da página de lista e formulário de atividades

---
+++ src/app/core/features/atividade/pages/atividade-form/atividade-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-atividade-form',
+  styleUrl: './atividade-form.scss',
+  templateUrl: './atividade-form.html',
+})
+export class AtividadeForm {}
