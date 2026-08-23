82f440f08dcbc7abf71244d459b110bbf9292943
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2026-08-23 14:37:35 -0300

    feature/atividade-pages -Ajustando component para routear para detalhes atividade

--- src/app/core/features/dashboard/components/today-activities/today-activities.ts
+++ src/app/core/features/dashboard/components/today-activities/today-activities.ts
@@ -4,9 +4,10 @@ import { DatePipe } from '@angular/common';
 import { Atividade } from '../../../atividade/models/atividade.model';
 import { ChartHours } from "../chart-hours/chart-hours";
 import { EmpresaModel } from '../../../empresas/models/empresa.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [CommonModule, DatePipe, ChartHours],
+  imports: [CommonModule, DatePipe, ChartHours, RouterLink],
   selector: 'app-today-activities',
   styleUrl: './today-activities.scss',
   templateUrl: './today-activities.html',
