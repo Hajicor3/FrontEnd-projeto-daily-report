8fca563d8760656d20fcb7bdb91a8aaae0a36111
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2026-08-25 17:50:09 -0300

    feat: Adicionando responsividade das páginas em celulares

--- src/app/core/shared/components/slidebar/slidebar.ts
+++ src/app/core/shared/components/slidebar/slidebar.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   selector: 'app-slidebar',
   styleUrl: './slidebar.scss',
   templateUrl: './slidebar.html',
 })
-export class Slidebar {}
+export class Slidebar {
+  isOpen = signal(false);
+
+  toggleSidebar() {
+    this.isOpen.update((value) => !value);
+  }
+
+  closeSidebar() {
+    this.isOpen.set(false);
+  }
+}
